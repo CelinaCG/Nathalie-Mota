ed30feddab11c5d6041661371029da4920666242
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-09-18 20:37:42 +0200

    Ajout footer

--- footer.php
+++ footer.php
@@ -1,1 +1,10 @@
+<footer class="site__footer">
+    <?php wp_nav_menu( array('theme_location' => 'footer')); ?>
+    <ul>
+        <!-- <li><a href="#">Mentions Légales</a></li>
+        <li><a href="#">Vie privée</a></li> -->
+        <li><p>Tous droits réservés</p></li>
+    </ul>
+    
+</footer>
 <?php wp_footer(); ?>
