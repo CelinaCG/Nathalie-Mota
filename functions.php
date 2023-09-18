f3a65c168a92d1b4102a64f96c6abd5830f01ab8
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-09-18 11:24:05 +0200

    Ajout CSS, Javascript et Jquery

--- functions.php
+++ functions.php
@@ -8,3 +8,15 @@ add_theme_support( 'title-tag' );
 
 // Support logo
 add_theme_support( 'custom-logo' );
+
+// Ajout style CSS et script JQuery
+function ajout_CSS_script() {
+    // JQuery
+    wp_enqueue_script('jquery');
+    // JS
+    wp_enqueue_script('script', get_template_directory_uri() . '/script.js', array('jquery'), '1.0', true);
+    // CSS
+    wp_enqueue_style('style', get_stylesheet_uri(), array(), '1.0');
+
+}
+add_action( 'wp_enqueue_scripts', 'ajout_CSS_script' );
