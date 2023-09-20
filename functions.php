b375b7bb3ad1bb5a1c0867c0766dda4441f3e2f5
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-09-20 21:50:10 +0200

    Changement de chemin fichiers JS et CSS

--- functions.php
+++ functions.php
@@ -14,9 +14,9 @@ function ajout_CSS_script() {
     // JQuery
     wp_enqueue_script('jquery');
     // JS
-    wp_enqueue_script('script', get_template_directory_uri() . '/script.js', array('jquery'), '1.0', true);
+    wp_enqueue_script('script', get_template_directory_uri() . '/assets/js/script.js', array('jquery'), '1.0', true);
     // CSS
-    wp_enqueue_style('style', get_stylesheet_uri(), array(), '1.0');
+    wp_enqueue_style('style', get_template_directory_uri() . '/assets/css/style.css');
 
 }
 add_action( 'wp_enqueue_scripts', 'ajout_CSS_script' );
