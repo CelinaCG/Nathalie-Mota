c64712969314e47c3994d0a51374f3f804433d11
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-09-20 21:48:55 +0200

    Ajout classe

--- footer.php
+++ footer.php
@@ -1,10 +1,8 @@
 <footer class="site__footer">
     <?php wp_nav_menu( array('theme_location' => 'footer')); ?>
-    <ul>
-        <!-- <li><a href="#">Mentions Légales</a></li>
-        <li><a href="#">Vie privée</a></li> -->
-        <li><p>Tous droits réservés</p></li>
-    </ul>
+    
+    <p class="droits-reserves">Tous droits réservés</p>
+  
     <?php get_template_part('template-parts/modale', '');?>
 </footer>
 <?php wp_footer(); ?>
