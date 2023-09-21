8c7e389afae5bfd1929969e0cd3120a22458d8b0
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-09-21 11:20:05 +0200

    Suppression commentaire

--- template-parts/modale.php
+++ template-parts/modale.php
@@ -6,8 +6,7 @@
     
     <!-- Contenu modale -->
     <div class="modal-content">
-        <img class="header-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/Contact-header.png">
-        <!-- <span class="close">x</span> -->
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/contact.png">
         <?php echo do_shortcode('[contact-form-7 id="bf60a67" title="Formulaire de contact"]'); ?>
     </div>
 
