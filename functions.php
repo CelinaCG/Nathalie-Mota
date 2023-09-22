77c929b8ab80b563bf0dbd34d52a3d92fd994107
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-09-22 18:23:05 +0200

    MAJ chemin logo

--- functions.php
+++ functions.php
@@ -7,7 +7,27 @@ add_theme_support( 'post-thumbnails' );
 add_theme_support( 'title-tag' );
 
 // Support logo
-add_theme_support( 'custom-logo' );
+
+function montheme_customize_register($wp_customize)
+{
+	// Ajout d'une section pour le logo personnalisé
+	$wp_customize->add_section('montheme_logo_section', array(
+		'title'      => __('Logo personnalisé', 'montheme'),
+		'priority'   => 30,
+	));
+
+	// Ajout de la fonctionnalité de logo personnalisé
+	$wp_customize->add_setting('montheme_logo');
+
+	// Ajout du contrôle pour téléverser le logo personnalisé
+	$wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'montheme_logo', array(
+		'label'    => __('Téléverser votre logo', 'montheme'),
+		'section'  => 'montheme_logo_section',
+		'settings' => 'montheme_logo',
+	)));
+}
+add_action('customize_register', 'montheme_customize_register');
+
 
 // Ajout style CSS et script JQuery
 function ajout_CSS_script() {
@@ -16,7 +36,7 @@ function ajout_CSS_script() {
     // JS
     wp_enqueue_script('script', get_template_directory_uri() . '/assets/js/script.js', array('jquery'), '1.0', true);
     // CSS
-    wp_enqueue_style('style', get_template_directory_uri() . '/assets/css/style.css');
+    wp_enqueue_style('style', get_stylesheet_uri(), array(), '1.0');
 
 }
 add_action( 'wp_enqueue_scripts', 'ajout_CSS_script' );
