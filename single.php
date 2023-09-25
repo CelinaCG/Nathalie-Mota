4feefd71f96250267965c6918e9024a90dd18cb9
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-09-25 21:56:57 +0200

    Ajout début de description photo

--- single.php
+++ single.php
@@ -1,3 +1,11 @@
 <?php get_header(); ?>
-	<h1>SINGLE</h1>
+	<h2><?php the_field('title') ?></h2>
+	<p class="description-photo">RÉFÉRENCE : <?php the_field('reference') ?></p>
+	<!-- Catégorie -->
+	<p class="description-photo"><?php get_the_terms(get_the_ID(),'categorie')[0]->name ?></p>
+	<!-- Format -->
+	<p class="description-photo"><?php get_the_terms(get_the_ID(),'format')?></p>
+	<p class="description-photo">TYPE : <?php the_field('type') ?></p>
+	<!-- Année -->
+	<p class="description-photo"><?php get_the_date('Y') ?></p>
 <?php get_footer(); ?>
