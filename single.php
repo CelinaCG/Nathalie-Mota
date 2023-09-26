4bfeee47f33e5007d1960695afb30006f56e7b5a
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-09-26 22:13:18 +0200

    Modif code catégorie et format

--- single.php
+++ single.php
@@ -3,14 +3,14 @@
 	<h2><?php echo the_title() ?></h2>
 	<p class="description-photo">RÉFÉRENCE : <?php echo get_field('reference') ?></p>
 	<!-- Catégorie -->
-	<p class="description-photo"><?php echo get_the_terms(get_the_ID(),'categorie') ?></p>
+	<p class="description-photo">CATÉGORIE : <?php echo get_the_terms(get_the_ID(),'categorie')[0]->name ?></p>
 	<!-- Format -->
-	<p class="description-photo"><?php echo get_the_terms(get_the_ID(),'format')?></p>
+	<p class="description-photo">FORMAT : <?php echo get_the_terms(get_the_ID(),'format')[0]->name ?></p>
 	<p class="description-photo">TYPE : <?php echo get_field('type') ?></p>
 	<!-- Année -->
 	<p class="description-photo">ANNÉE : <?php echo get_the_date('Y') ?></p>
 	</section>
 
-	<section
+	<section></section>
 	
 <?php get_footer(); ?>
