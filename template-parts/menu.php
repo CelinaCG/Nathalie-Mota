d1b4e06aef66ef89d689a3002f267dca6787623a
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-09-26 17:44:04 +0200

    Ajout nav du menu burger

---
+++ template-parts/menu.php
@@ -0,0 +1,9 @@
+<!-- Menu burger -->
+
+<nav id="side-nav" class="sidenav-burger m-burger">
+    <ul>
+        <li><a href="<?php echo esc_url(home_url('/')) ?>">Accueil</a></li>
+        <li><a href="#">À propos</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+</nav>
