df447ffeb7a0eff8cf2f21ce23edf688419565a5
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-09-26 17:47:07 +0200

    Ajout echo pour appel des données PHP

--- single.php
+++ single.php
@@ -1,11 +1,16 @@
 <?php get_header(); ?>
-	<h2><?php the_field('title') ?></h2>
-	<p class="description-photo">RÉFÉRENCE : <?php the_field('reference') ?></p>
+	<section>
+	<h2><?php echo the_title() ?></h2>
+	<p class="description-photo">RÉFÉRENCE : <?php echo get_field('reference') ?></p>
 	<!-- Catégorie -->
-	<p class="description-photo"><?php get_the_terms(get_the_ID(),'categorie')[0]->name ?></p>
+	<p class="description-photo"><?php echo get_the_terms(get_the_ID(),'categorie') ?></p>
 	<!-- Format -->
-	<p class="description-photo"><?php get_the_terms(get_the_ID(),'format')?></p>
-	<p class="description-photo">TYPE : <?php the_field('type') ?></p>
+	<p class="description-photo"><?php echo get_the_terms(get_the_ID(),'format')?></p>
+	<p class="description-photo">TYPE : <?php echo get_field('type') ?></p>
 	<!-- Année -->
-	<p class="description-photo"><?php get_the_date('Y') ?></p>
+	<p class="description-photo">ANNÉE : <?php echo get_the_date('Y') ?></p>
+	</section>
+
+	<section
+	
 <?php get_footer(); ?>
