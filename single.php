93bcd002a95854441e91fa105e1fccc88bd786ea
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-09-27 20:42:44 +0200

    Ajout description et photo

--- single.php
+++ single.php
@@ -1,16 +1,20 @@
 <?php get_header(); ?>
-	<section>
+<div class="detail-photo">
+
+
+	<section class="bordure-description">
 	<h2><?php echo the_title() ?></h2>
 	<p class="description-photo">RÉFÉRENCE : <?php echo get_field('reference') ?></p>
-	<!-- Catégorie -->
 	<p class="description-photo">CATÉGORIE : <?php echo get_the_terms(get_the_ID(),'categorie')[0]->name ?></p>
-	<!-- Format -->
 	<p class="description-photo">FORMAT : <?php echo get_the_terms(get_the_ID(),'format')[0]->name ?></p>
 	<p class="description-photo">TYPE : <?php echo get_field('type') ?></p>
-	<!-- Année -->
 	<p class="description-photo">ANNÉE : <?php echo get_the_date('Y') ?></p>
 	</section>
 
-	<section></section>
+	<section>
+		<!-- Affichage de la photo -->
+		<img class="photo" src="<?php echo get_the_post_thumbnail (); ?>
+	</section>
+</div>	
 	
 <?php get_footer(); ?>
