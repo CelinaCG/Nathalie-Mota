9fce036633e4753b95ae8944f73ace917d995bda
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-09-28 21:40:03 +0200

    Ajout code des flèches

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,3 +44,45 @@ $thumbnail_id = get_post_thumbnail_id();
 $thumbnail_url = wp_get_attachment_image_src($thumbnail_id, 'custom-size');
 // Récupération de l'attribut ALT de l'image mise en avant pour une meilleure accessibilité
 $thumbnail_alt = get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', true);
+
+// Evènement flèches
+
+// Flèche gauche
+
+let flecheGauche = document.querySelector(".arrow-left");
+// Clickflèche désigne la variable commune aux deux flèches pour permettre l'incrémentation ou la décrémentation de la valeur numérique du contenu de la table "slides".
+let clickFleche = 0;
+flecheGauche.addEventListener("click", function() {
+	// Décrémentation de la position de l'image/slide
+	clickFleche--;
+	// Condition if pour définir position de la slide après incrémentation
+	if (clickFleche < 0) {
+		// Définir le défilement et retour vers l'image initiale via -1 pour aller vers la dernière image de la liste du tableau et pas au-delà car le rang ne va que jusqu'à 3 (0 à 3)
+		clickFleche = slides.length -1;
+	}
+
+	// Changer l'image
+	const imageElement = document.querySelector(".banner-img");
+	console.log(clickFleche);
+	// + est une concaténation et pointe la propriété image à la position de sa valeur via "clickflèche"
+	imageElement.src = "./assets/images/slideshow/" + slides[clickFleche].image;
+
+});
+	
+
+// Flèche droite
+
+let flecheDroite = document.querySelector(".arrow-right");
+flecheDroite.addEventListener("click", function() {
+	console.log("Bouton droit cliqué !")
+	// Incrémentation de la position de l'image
+	clickFleche++;
+	if (clickFleche > slides.length -1) {
+		clickFleche = 0;
+	}
+
+	// Changer l'image
+	const imageElement = document.querySelector(".banner-img");
+	imageElement.src = "./assets/images/slideshow/" + slides[clickFleche].image;
+	
+});
