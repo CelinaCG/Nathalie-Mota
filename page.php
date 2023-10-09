736c950da9f6aa005484b4ee48b309a9e5be3aca
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-10-09 20:32:14 +0200

    Essai code hero header

--- page.php
+++ page.php
@@ -1,11 +1,16 @@
 <?php get_header(); ?>
 
 	<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+		<!-- Appel template-part du hero header -->
+		<?php get_template_part('template-parts/photo' , 'block'); ?>
     
     	<h1><?php the_title(); ?></h1>
     
     	<?php the_content(); ?>
+	
 
 	<?php endwhile; endif; ?>
+
+
 
 <?php get_footer(); ?>
