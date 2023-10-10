04b76031853795d4fdcc12fe50e17f885a529064
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-10-10 21:00:20 +0200

    Ajout WP_query

--- template-parts/photo-block.php
+++ template-parts/photo-block.php
@@ -1,29 +1,20 @@
 <!-- Bloc d'affichage d'une photo de la liste pour page accueil -->
 
 <section class="hero-header">
-    <?php query_posts('showposts=1&orderby=rand'); ?>
-    <a href="<?php the_permalink(); ?>">
-    <?php if ( has_post_thumbnail() ) : ?><?php the_post_thumbnail('large'); ?>
+<?php query_posts(array(
+        'post_type' => 'photo',
+        'showposts' => '1',
+        'orderby' => 'rand',
+    )); ?>
     
-    <?php endif;  ?>
-
-    
-	<!-- // Create WordPress Query with 'orderby' set to 'rand' (Random) --> 
-    <?php $the_query = new WP_Query( array ( 'orderby' => 'rand', 'posts_per_page' => '1' ) );
-    // output the random post
-    while ( $the_query->have_posts() ) : $the_query->the_post();
-        // echo '<li>';
-        // the_title();
-        // echo '</li>';   
-        // echo <a href="<?php the_permalink(); ?>
-        ">
-        <?php 
-        // echo the_post_thumbnail('large'); 
-     
-    endwhile;
-
-    // // Reset Post Data
-    // wp_reset_postdata(); 
-    ?>
+    <?php if(have_posts()):
+        while (have_posts()): 
+            the_post(); ?>
+            <img class="photo-aleatoire" src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title_attribute(); ?>" 
+        <?php endwhile; 
+        endif; ?> >
+    <img class="photoevent" src="<?php echo get_template_directory_uri(); ?> '/assets/images/photoevent.jpeg' " alt="photoevent">
+        
+        
 
 </section>
