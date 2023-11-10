1e0177c351ef12b817a3d4acdf4acbead5107a14
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-11-10 22:03:25 +0100

    Ajout code pour filtres

--- template-parts/photo-block.php
+++ template-parts/photo-block.php
@@ -1,1 +1,20 @@
-<!-- Affichage photos apparentées ? -->
+<!-- Affichage photos accueil -->
+
+<?php 
+    $photos = new WP_Query([
+        'post_type' => 'photo',
+        'posts_per_page' => -1,
+        'order_by' => 'date',
+        'order' => 'desc',
+    ]);
+?>
+
+<?php if($photos->have_posts()): ?>
+    <ul class="photo-tiles">
+        <?php
+            while($photos->have_posts()) : $photos->the_post();
+            endwhile;
+        ?>
+    </ul>
+    <?php wp_reset_postdata(); ?>
+<?php endif; ?>
