bc39dd439bae773f0918ec7d59cdc5501713afc5
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-11-10 14:51:33 +0100

    Modif boucle pour affichage image header

--- template-parts/custom-header.php
+++ template-parts/custom-header.php
@@ -1,19 +1,40 @@
 <!-- Bloc d'affichage d'une photo de la liste pour page accueil -->
 
 <section class="hero-header">
-    <?php query_posts(array(
-        'post_type' => 'photo',
-        'showposts' => '1',
+    <?php 
+    $the_query = new WP_Query(array( 
         'orderby' => 'rand',
-    )); ?>
+        'posts_per_page' => 1 ,
+        'post_type' => 'photo'
+    ));
     
-    <?php if(have_posts()):
-        while (have_posts()): 
-            the_post(); ?>
-            <img class="photo-aleatoire" src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title_attribute(); ?>" 
-        <?php endwhile; 
-        endif; ?> >
-    <img class="photoevent" src="<?php echo get_template_directory_uri(); ?> '/assets/images/photoevent.jpeg' " alt="photoevent">
+    
+    if($the_query -> have_posts()):
+
+    // <!-- Boucle -->
+    
+        while ($the_query -> have_posts()) {
+            $the_query -> the_post();
+            the_post_thumbnail(); 
+            wp_reset_postdata();
+            exit;
+        } 
+
+        
+        // endwhile; 
+        // Restore original Post Data.
+        wp_reset_postdata();
+
+        // endif; 
+        ?>
+        <?php else : ?>
+	<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+    <?php endif; ?>
+
+
+    <!-- <img class="photoevent" src="<?php 
+    // echo get_template_directory_uri() . '/assets/images/photoevent.jpeg' 
+    ?> " alt="photoevent"> -->
         
         
 
