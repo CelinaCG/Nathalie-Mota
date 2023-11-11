009d48f049f73e13df13e33d54d3aa904c0e04ea
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-11-11 17:56:07 +0100

    Boucle mis en commentaire pour éviter boucle infinie

--- page.php
+++ page.php
@@ -1,33 +1,38 @@
 <?php get_header(); ?>
 
-	<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+	<?php 
+	// if( have_posts() ) : while( have_posts() ) : the_post(); 
+	?>
 		<!-- Appel template-part du hero header -->
 		<?php 
-		get_template_part('template-parts/custom' , 'header');
+		// get_template_part('template-parts/custom' , 'header');
 		?>
     
     	<h1><?php the_title(); ?></h1>
     
-    	<?php the_content(); ?>
+    	<?php 
+		the_content(); 
+		?>
 		<!-- Boucle principale -->
 
 	
 	
-		<?php new WP_Query( array( 'post_type' => 'post' ) );
+		<?php 
+		// new WP_Query( array( 'post_type' => 'post' ) );
 	
 	// Boucle personnalisée
-	if( have_posts() ) : while(have_posts() ) : 
-		the_post();
+	// if( have_posts() ) : while(have_posts() ) : 
+	// 	the_post();
         
 		// the_title(); // Titre de chaque article
         // the_content(); // Contenu de chaque article
-		the_post_thumbnail();
-		endwhile; endif;
+		// the_post_thumbnail();
+		// endwhile; endif;
 		
 
 		// the_content(); // Contenu de la page
 
-	endwhile; endif; wp_reset_postdata(); // On réinitialise les données
+	// endwhile; endif; wp_reset_postdata(); // On réinitialise les données
 	?>
 	
 
