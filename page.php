923131106647fe87c366d3998c220ab2fa6716d2
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-11-13 11:16:56 +0100

    Ajout template part pour bannière aléatoire

--- page.php
+++ page.php
@@ -1,45 +1,33 @@
 <?php get_header(); ?>
-	<section>
-	<?php 
-	// if( have_posts() ) : while( have_posts() ) : the_post(); 
-	?>
-		<!-- Appel template-part du hero header -->
-		<?php 
-		// get_template_part('template-parts/custom' , 'header');
-		?>
+	<section class="container">
 	
-		<!-- Bannière accueil -->
-		<div class="section-single banner-style">
+		<!-- Appel template-part du hero header -->
+		<div class="banner-style">
+			<?php get_template_part('template-parts/custom' , 'header'); ?>
 			<h1 class="title"><?php the_title(); ?></h1>
 
-			<img src="<?php echo get_template_directory_uri() . '/assets/images/photoevent.jpeg'; ?> " alt="bannière">
+			<!-- <img src=" -->
+			<?php 
+			// echo get_template_directory_uri() . '/assets/images/photoevent.jpeg'; 
+			?> 
+			<!-- " alt="bannière"> -->
 		</div>
-    	
+		?>
+
     
     	<?php 
 		the_content(); 
 		?>
-		<!-- Boucle principale -->
+	
 
 	
 	
-		<?php 
-		// new WP_Query( array( 'post_type' => 'post' ) );
-	
-	// Boucle personnalisée
-	// if( have_posts() ) : while(have_posts() ) : 
-	// 	the_post();
-        
-		// the_title(); // Titre de chaque article
-        // the_content(); // Contenu de chaque article
-		// the_post_thumbnail();
-		// endwhile; endif;
 		
+		
+	
 
-		// the_content(); // Contenu de la page
-
-	// endwhile; endif; wp_reset_postdata(); // On réinitialise les données
-	?>
+	
+	
 	
 
  
