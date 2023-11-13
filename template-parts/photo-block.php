af55b248d7edcf9f4c87ff3ee4999a82c25b1900
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-11-13 21:25:36 +0100

    Modif requète photos apparentées

--- template-parts/photo-block.php
+++ template-parts/photo-block.php
@@ -1,20 +1,21 @@
-<!-- Affichage photos accueil -->
+<!-- Affichage photos apparentées -->
 
 <?php 
-    $photos = new WP_Query([
-        'post_type' => 'photo',
-        'posts_per_page' => -1,
+    $the_query = new WP_Query(array(
+        'post_type' => 'photos',
+        'posts_per_page' => 2,
         'order_by' => 'date',
         'order' => 'desc',
-    ]);
-?>
+    ));
 
-<?php if($photos->have_posts()): ?>
-    <ul class="photo-tiles">
-        <?php
-            while($photos->have_posts()) : $photos->the_post();
-            endwhile;
-        ?>
-    </ul>
-    <?php wp_reset_postdata(); ?>
+    if($the_query->have_posts()):
+    while ($the_query -> have_posts()) {
+        $the_query -> the_post();
+        the_post_thumbnail(); 
+    } 
+    wp_reset_postdata();
+?>
+<?php else : ?>
+<p><?php esc_html_e( 'Désolé, il n\'y a aucun post qui correspond à vos critères.' ); ?></p>
 <?php endif; ?>
+
