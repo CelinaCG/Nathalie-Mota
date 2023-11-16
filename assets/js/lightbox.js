6d4060147c24dae8c4a6efe64be5fa127930f8ac
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-11-16 16:58:45 +0100

    Création fichier JS pour lightbox

---
+++ assets/js/lightbox.js
@@ -0,0 +1,16 @@
+var $lightbox = $('.lightbox'); // L'élément HTML
+
+// Ouvrir la lightbox
+$('.col-photo img').click(function(e) {
+    e.preventDefault(); // On empêche le changement de page
+    var url = $(this).attr('href'); // On récupère l'URL de l'image dans href
+
+    // On applique l'image en fond
+    $lightbox.css('background-image', 'url(' + url + ')'); 
+    $lightbox.fadeIn(); // Et on fait apparaitre la lightbox
+});
+
+// Fermer la lightbox
+$lightbox.click(function () {
+    $lightbox.fadeOut();
+});
