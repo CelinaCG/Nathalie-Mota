df761e6258510747a73e9204c530d0a32928b852
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-11-18 21:39:03 +0100

    Modif div hover

--- template-parts/photo-block.php
+++ template-parts/photo-block.php
@@ -8,43 +8,41 @@
 <!-- Affichage photos apparentées -->
 <div class="col-photo">
 
-<?php 
-    $current_category = get_the_terms(get_the_ID(), 'categorie');
-    $the_query = new WP_Query(array(
-        'post_type' => 'photos',
-        'posts_per_page' => 2,
-        'orderby' => 'rand',
-        'tax_query' => array(
-            array(
-                // Prendre les images de la taxonomie de "catégorie"
-                'taxonomy' => 'categorie',
-                // Prendre l'ID de la photo affichée sur le single.php
-                'field' => 'term_id',
-                // Utiliser la catégorie actuelle
-                'terms' => $current_category[0]->term_id,
+    <?php 
+        $current_category = get_the_terms(get_the_ID(), 'categorie');
+        $the_query = new WP_Query(array(
+            'post_type' => 'photos',
+            'posts_per_page' => 2,
+            'orderby' => 'rand',
+            'tax_query' => array(
+                array(
+                    // Prendre les images de la taxonomie de "catégorie"
+                    'taxonomy' => 'categorie',
+                    // Prendre l'ID de la photo affichée sur le single.php
+                    'field' => 'term_id',
+                    // Utiliser la catégorie actuelle
+                    'terms' => $current_category[0]->term_id,
+                ),
             ),
-        ),
-    ));
+        ));
 
-    if($the_query->have_posts()):
-    while ($the_query -> have_posts()) {
-        $the_query -> the_post();
-        echo '<div class="hover-photo">';
-        the_post_thumbnail(); 
-        echo '</div>';
-        echo '<div id="myHover" class="lightbox-hover">';
-        // the_post_thumbnail();
-        echo '<img class="oeil" src="' . get_template_directory_uri() . '/assets/images/Icon_eye.png" >';
-        echo '<img class="zoom" src="' . get_template_directory_uri() . '/assets/images/Icon_fullscreen.png" >';
-        echo '<p>RÉFÉRENCE : <?php echo $refPhoto ?></p>';
-        echo '<p>CATÉGORIE : <?php echo $catname ?></p>';
-        echo '</div>';
-
-        // Ajouter hoover qui affiche icones oeil et agradissement, ref photo et categorie
-    } 
-    wp_reset_postdata();
-?>
-<?php else : ?>
-<p><?php esc_html_e( 'Désolé, il n\'y a aucun post qui correspond à vos critères.' ); ?></p>
-<?php endif; ?>
+        if($the_query->have_posts()):
+        while ($the_query -> have_posts()) {
+            $the_query -> the_post();
+            // Mise en place du hover
+            echo '<div class="hover-photo">';
+            the_post_thumbnail(); 
+            echo '<div id="myHover" class="lightbox-hover">';
+            echo '<img class="oeil" src="' . get_template_directory_uri() . '/assets/images/Icon_eye.png" >';
+            echo '<img class="zoom" src="' . get_template_directory_uri() . '/assets/images/Icon_fullscreen.png" >';
+            echo '<p>RÉFÉRENCE : <?php echo $refPhoto ?></p>';
+            echo '<p>CATÉGORIE : <?php echo $catname ?></p>';
+            echo '</div>';
+            echo '</div>';
+        } 
+        wp_reset_postdata();
+    ?>
+    <?php else : ?>
+    <p><?php esc_html_e( 'Désolé, il n\'y a aucun post qui correspond à vos critères.' ); ?></p>
+    <?php endif; ?>
 </div>
