9689608f818587dd372d2952c370b2e2cc6f9bc7
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-11-20 22:20:48 +0100

    Ajout template part lightbox

--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
 <!-- Fermeture du container -->
 </main>
 <footer class="site__footer">
-    <!-- Lightbox -->
-    <div class="lightbox"></div>
     
     <?php wp_nav_menu( array('theme_location' => 'footer')); ?>
     
     <p class="droits-reserves">Tous droits réservés</p>
   
     <?php get_template_part('template-parts/modale', '');?>
+
+    <?php get_template_part('template-parts/lightbox', '');?>
 </footer>
 <?php wp_footer(); ?>
 
