6bf7871490928d0a411a827ce2a622c42b04952f
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-11-21 15:37:08 +0100

    Suppression classe bannière header

--- page.php
+++ page.php
@@ -2,16 +2,14 @@
 	<section class="container">
 	
 		<!-- Appel template-part du hero header -->
-		<div class="banner-style">
+		<!-- <div class="banner-style"> -->
 			<?php get_template_part('template-parts/custom' , 'header'); ?>
 			<h1 class="title"><?php the_title(); ?></h1>
 
-			<!-- <img src=" -->
-			<?php 
-			// echo get_template_directory_uri() . '/assets/images/photoevent.jpeg'; 
-			?> 
-			<!-- " alt="bannière"> -->
-		</div>
+		
+	
+		
+		<!-- </div> -->
 
     
     	<?php 
