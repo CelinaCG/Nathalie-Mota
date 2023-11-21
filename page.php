dba08f1ae8673d968b342c023ce0341fbdbde999
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-11-21 21:12:30 +0100

    Suppression H1

--- page.php
+++ page.php
@@ -2,16 +2,9 @@
 	<section class="container">
 	
 		<!-- Appel template-part du hero header -->
-		<!-- <div class="banner-style"> -->
-			<?php get_template_part('template-parts/custom' , 'header'); ?>
-			<h1 class="title"><?php the_title(); ?></h1>
-
-		
 	
-		
-		<!-- </div> -->
-
-		
+		<?php get_template_part('template-parts/custom' , 'header'); ?>
+			
 		<!-- Section filtres pour page d'acceuil -->
 
 		<!-- Catégories -->
