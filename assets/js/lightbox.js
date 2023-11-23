7a5b2ef37bbbd486398f699753464fbd2ebea9a7
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-11-23 21:21:24 +0100

    Modif requète lightbox

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -1,26 +1,22 @@
-// Prendre le lightbox et les éléments de l'overlay
-var lightbox = document.getElementById('myLightbox');
+jQuery(document).ready(function() {
+    // Prendre le lightbox et les éléments de l'overlay
+    var lightbox = document.querySelector('#myLightbox');
+    
+    // Activation du lightbox par l'image zoom
+    var zoomLightbox = document.querySelectorAll('.lightbox-open');
 
-// Activation du lightbox par l'image zoom
-var zoomLightbox = document.querySelectorAll('.lightbox-open');
-// foreach
-zoomLightbox.forEach(function(element) {
-    element.addEventListener('click', function() {
-        // Prendre l'élément du lightbox
-        var lightbox = document.getElementById('myLightbox');
-        // Rendre cet élément actif
-        lightbox.classList.add('active');
-    })
-}
-);
+    // Ouverture de la lighbox au clic sur l'image zoom
+    zoomLightbox.forEach(function(zoom, index) {
+        zoom.addEventListener('click', function(e) {
+            // Eviter le comportement par défaut de l'évènement (ex: éviter que le navigateur accède à une autre page que la page actuelle )
+            e.preventDefault();
+            // Affichage du lightbox
+            lightbox.style.display = "block";
+        });
+    });
+});
 
-
-// Ouverture de la lighbox au clic sur l'image zoom
-// zoomLightbox.onclick = function() {
-//     lightbox.style.display = "block";
-// }
-
-// Fermeture de la modale au clic sur la croix
+// Fermeture de la lightbox au clic sur la croix
 window.onclick = function(event) {
     if (event.target == lightbox) {
         lightbox.style.display = "none";
