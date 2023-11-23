f547cc907e745aef2d8e4407152de6f602be2499
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-11-23 23:56:33 +0100

    Ajout classes éléments lightbox

--- template-parts/lightbox.php
+++ template-parts/lightbox.php
@@ -13,18 +13,26 @@
         <div class="zoom-image">
         <img src="<?php echo get_the_post_thumbnail_url(); ?>">
         </div>
-        <img class="exit-lighbox" src="<?php echo get_template_directory_uri() . '/assets/images/white-cross.png' ?>">
+        <img class="exit-lightbox" src="<?php echo get_template_directory_uri() . '/assets/images/white-cross.png' ?>">
         <div class="fleches">
             <!-- Flèche gauche -->
-            <p>Précédente<img class="flechegauche-overlay-inactive" src="<?php echo get_template_directory_uri() . '/assets/images/short-arrow-left.png' ?>">
+            <p class="previous-lightbox">Précédente<img class="flechegauche-overlay-inactive" src="<?php echo get_template_directory_uri() . '/assets/images/short-arrow-left.png' ?>">
             <img class="flechegauche-overlay-active" src="<?php echo get_template_directory_uri() . '/assets/images/long-arrow-left.png' ?>"></p>
 
             <!-- Flèche droite -->
-            <p>Suivante<img class="flechedroite-overlay-inactive" src="<?php echo get_template_directory_uri() . '/assets/images/short-arrow-right.png' ?>">
+            <p class="next-lightbox">Suivante<img class="flechedroite-overlay-inactive" src="<?php echo get_template_directory_uri() . '/assets/images/short-arrow-right.png' ?>">
             <img class="flechedroite-overlay-active"  src="<?php echo get_template_directory_uri() . '/assets/images/long-arrow-right.png' ?>"></p>
         </div>
-        <p class="legend">RÉFÉRENCE : <?php echo $refPic ?></p>
-        <p class="legend">CATÉGORIE : <?php echo $categoname ?></p>
+        <div class="legend-lb-align">
+            <div>
+                <p class="legend">RÉFÉRENCE : <?php echo $refPic ?></p>
+            </div>
+            <div>
+            <p class="legend">CATÉGORIE : <?php echo $categoname ?></p>
+            </div>       
+      
+        </div>
+       
         
         
     </div>
