291aac343a17063ffec26d62af8c9beb2bb366a1
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-11-23 20:09:18 +0100

    Modif permalink

--- template-parts/photo-block.php
+++ template-parts/photo-block.php
@@ -33,7 +33,7 @@
             echo '<div class="lightbox-hover">';
             // Récupération de l'image + référence + catégorie
             echo '<img class="zoom lightbox-open" data-category='. $catname .' data-reference='. $refPhoto .' data-image='. get_the_post_thumbnail_url() .'  src="' . get_template_directory_uri() . '/assets/images/Icon_fullscreen.png" >';
-            echo '<a href=" '. the_permalink() . '"><img class="oeil" src="' . get_template_directory_uri() . '/assets/images/Icon_eye.png" ></a>';
+            echo '<a href=" '. get_the_permalink() . '"><img class="oeil" src="' . get_template_directory_uri() . '/assets/images/Icon_eye.png" ></a>';
             echo '<div class="legend-align">';
             echo '<div class="legend ref-photo">' . $refPhoto . '</div>';
             echo '<div class="legend cat">' . $catname . '</div>';
