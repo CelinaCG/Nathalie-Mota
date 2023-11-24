7315d8dfd937e69a180aef75e6df5ecc167fb1fd
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-11-24 17:44:22 +0100

    MAJ JS lightbox

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -1,11 +1,10 @@
+// Prendre le lightbox et les éléments de l'overlay
+var lightbox = document.querySelector('#myLightbox');
 jQuery(document).ready(function() {
-    // Prendre le lightbox et les éléments de l'overlay
-    var lightbox = document.querySelector('#myLightbox');
-    
     // Activation du lightbox par l'image zoom
     var zoomLightbox = document.querySelectorAll('.lightbox-open');
 
-    // Ouverture de la lighbox au clic sur l'image zoom
+    // Ouverture de la lightbox au clic sur l'image zoom
     zoomLightbox.forEach(function(zoom, index) {
         zoom.addEventListener('click', function(e) {
             // Eviter le comportement par défaut de l'évènement (ex: éviter que le navigateur accède à une autre page que la page actuelle )
@@ -17,8 +16,7 @@ jQuery(document).ready(function() {
 });
 
 // Fermeture de la lightbox au clic sur la croix
-window.onclick = function(event) {
-    if (event.target == lightbox) {
-        lightbox.style.display = "none";
-    }
-}
+document.querySelector('.exit-lightbox').addEventListener('click', function(e) {
+    e.preventDefault();
+    lightbox.style.display="none";
+});
