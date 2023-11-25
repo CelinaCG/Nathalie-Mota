d5ec4bbed6008f8474c7f0d0bbf6af6536620165
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-11-25 16:32:54 +0100

    Modif gallerie

--- template-parts/home-gallery.php
+++ template-parts/home-gallery.php
@@ -2,37 +2,59 @@
 <div class="col-photo">
 
     <?php 
-        $current_category = get_the_terms(get_the_ID(), 'categorie');
-        $the_query = new WP_Query(array(
+        $args = array(
             'post_type' => 'photos',
             'posts_per_page' => 12,
-        ));
+            'post__not_in' => array(get_the_ID()),
+            'paged' => 1
+        );
+        $query = new WP_Query($args);
 
-        if($the_query->have_posts()):
-        while ($the_query -> have_posts()) {
-            $the_query -> the_post();
-            // Variables de stockage
-            $refPhoto = get_field("reference");
-            $cat = get_the_terms($post, "categorie");
-            $catname = $cat[0]->name;	
-
-            // Mise en place du hover
-            echo '<div class="hover-photo">';
-            the_post_thumbnail(); 
-            echo '<div class="lightbox-hover">';
-            // Récupération de l'image + titre + catégorie
-            echo '<img class="zoom lightbox-open" data-category='. $catname .' data-reference='. $refPhoto .' data-image='. get_the_post_thumbnail_url() .'  src="' . get_template_directory_uri() . '/assets/images/Icon_fullscreen.png" >';
-            echo '<a href=" '. get_the_permalink() .' "><img class="oeil" src="' . get_template_directory_uri() . '/assets/images/Icon_eye.png" ></a>';
-            echo '<div class="legend-align">';
-            echo '<div class="legend ref-photo">' . get_the_title() . '</div>';
-            echo '<div class="legend cat">' . $catname . '</div>';
-            echo '</div>';
-            echo '</div>';
-            echo '</div>';
-        } 
-        wp_reset_postdata();
+        if ($query->have_posts()) :
+            while ($query->have_posts()) :
+                $query->the_post();
     ?>
-    <?php else : ?>
-    <p><?php esc_html_e( 'Désolé, il n\'y a aucun post qui correspond à vos critères.' ); ?></p>
-    <?php endif; ?>
+    <div class="hover-photo">
+        <a href="#">
+            <img class="lightbox-hover" data-category="<?php echo strip_tags(get_the_term_list(get_the_ID(), 'categorie')); ?>" data-reference="<?php echo get_field('reference', get_the_ID()); ?>" data-image="<?php echo get_the_post_thumbnail_url(); ?>" src="<?php echo get_template_directory_uri(); ?>/assets/images/Icon_fullscreen.png" alt="full_screen">
+        </a>
+        <a href="<?php echo get_the_permalink(get_the_ID()); ?>">
+            <img class="oeil" src="<?php echo get_template_directory_uri(); ?>/assets/images/Icon_eye.png" alt="oeil">
+        </a>
+        <div class="legend-align">
+            <div class="legend ref-photo"><?php echo get_field('reference', $post->ID); ?></div>
+            <div class="legend cat"><?php echo strip_tags(get_the_term_list($post->ID, 'categorie')); ?></div>
+        </div>
+
+    </div>
+    <?php
+        endwhile;
+        endif;
+        wp_reset_postdata();
+        // if($the_query->have_posts()):
+        // while ($the_query -> have_posts()) {
+        //     $the_query -> the_post();
+        //     // Variables de stockage
+        //     $refPhoto = get_field("reference");
+        //     $cat = get_the_terms($post, "categorie");
+        //     $catname = $cat[0]->name;	
+
+        //     // Mise en place du hover
+        //     echo '<div class="hover-photo">';
+        //     the_post_thumbnail(); 
+        //     echo '<div class="lightbox-hover">';
+        //     // Récupération de l'image + titre + catégorie
+        //     echo '<img class="zoom lightbox-open" data-category='. $catname .' data-reference='. $refPhoto .' data-image='. get_the_post_thumbnail_url() .'  src="' . get_template_directory_uri() . '/assets/images/Icon_fullscreen.png" >';
+        //     echo '<a href=" '. get_the_permalink() .' "><img class="oeil" src="' . get_template_directory_uri() . '/assets/images/Icon_eye.png" ></a>';
+        //     echo '<div class="legend-align">';
+        //     echo '<div class="legend ref-photo">' . get_the_title() . '</div>';
+        //     echo '<div class="legend cat">' . $catname . '</div>';
+        //     echo '</div>';
+        //     echo '</div>';
+        //     echo '</div>';
+        // // } 
+        // wp_reset_postdata();
+    ?>
+        
+
 </div>
