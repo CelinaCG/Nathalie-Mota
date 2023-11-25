4c2fcc0eacb7b87851a2358dea7428dc079904b1
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-11-25 16:29:19 +0100

    Suppression template

--- header.php
+++ header.php
@@ -25,8 +25,10 @@
                 <img class="menu_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/burger.png' ?>">
                 <img class="croix_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/cross.png' ?>">
             </div>
-            <?php get_template_part('template-parts/menu', ''); ?>
         </nav>
+        <?php 
+        // get_template_part('template-parts/menu', '');
+         ?>
     </header>
     
     <?php wp_body_open(); ?>
