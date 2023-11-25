5157ee345f14d9dfd2d1554361a1e5059307388d
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-11-25 16:30:13 +0100

    Modif filtres

--- page.php
+++ page.php
@@ -7,109 +7,118 @@
 			
 		<!-- Section filtres pour page d'acceuil -->
 		<div class="filters-gallery">
-		<!-- Catégories -->
-		<section class="main-filters">
-			<div class="ajax-filters">
-				<form id="ajax-filter">
-					<?php
-						$categories = get_terms(
-							array(
-								'taxonomy' => 'categorie',
-								'orderby' => 'rand',
-							) 
-						);
-						if( $categories ) :
-							?>
-								<select onfocus="this.size=5;" onblur="this.size=0;" onchange="this.size=1; this.blur()">
-									<option value="">CATÉGORIES</option>
-									<?php
-										foreach ( $categories as $category ) :
-											?><option value="<?php echo $category->term_id ?>"><?php echo $category->name ?></option><?php
-										endforeach;
-									?>
-								</select>
-							<?php
-						endif;
-					?>
-				</form>
+			<!-- Catégories -->
+			<section class="main-filters">
+				<div class="ajax-filters">
+					<form id="ajax-filter">
+						<select name="cat" id="catFilter">
+							<option value="all">CATÉGORIES</option>
+							<?php echo filtreCategorie(); ?>
+						</select>
+			
+					</form>
 		
+					<!-- Formats -->
+				
+					<form id="ajax-filter">
+						<select name="form" id="formFilter">
+							<option value="all">FORMATS</option>
+							<?php echo filtreFormat(); ?>
+						</select>
+					</form>
+				</div>
 
-				<!-- Formats -->
-			
-			
-				<form id="ajax-filter">
-					<?php
-						$categories = get_terms(
-							array(
-								'taxonomy' => 'format',
-								'orderby' => 'rand',
-							) 
-						);
-						if( $categories ) :
-							?>
-								<select>
-									<option value="">FORMATS</option>
-									<?php
-										foreach ( $categories as $category ) :
-											?><option value="<?php echo $category->term_id ?>"><?php echo $category->name ?></option><?php
-										endforeach;
-									?>
-								</select>
-							<?php
-						endif;
-					?>
-				</form>
-			</div>
+				<!-- Tri par date -->
 
-			<!-- Tri par date -->
-
-			<div class="ajax-filters">
-				<form id="ajax-filter">
-					
-					<select>
+				<div class="ajax-filters">
+					<form id="ajax-filter">
+						
+					<select name="tri" id="triFilter">
 						<option value="">TRIER PAR</option>
-						<option>Les plus anciennes au plus récentes</option>
-						<option>Les plus récentes au plus anciennes</option>
-					
+						<?php echo filtreOrderDirection(); ?>
 					</select>
-				</form>
+					</form>
+				</div>
+			</section>
+
+			<div class="col-photo">
+
+			<?php 
+				$args = array(
+					'post_type' => 'photos',
+					'posts_per_page' => 12,
+					'post__not_in' => array(get_the_ID()),
+					'paged' => 1
+				);
+				$query = new WP_Query($args);
+
+				if ($query->have_posts()) :
+				while ($query->have_posts()) :
+				$query->the_post();
+			?>
+			<div class="hover-photo">
+				<a href="#">
+					<img class="lightbox-hover" data-category="<?php echo strip_tags(get_the_term_list(get_the_ID(), 'categorie')); ?>" data-reference="<?php echo get_field('reference', get_the_ID()); ?>" data-image="<?php echo get_the_post_thumbnail_url(); ?>" src="<?php echo get_template_directory_uri(); ?>/assets/images/Icon_fullscreen.png" alt="full_screen">
+				</a>
+				<a href="<?php echo get_the_permalink(get_the_ID()); ?>">
+					<img class="oeil" src="<?php echo get_template_directory_uri(); ?>/assets/images/Icon_eye.png" alt="oeil">
+				</a>
+				<div class="legend-align">
+					<div class="legend ref-photo"><?php echo get_field('reference', $post->ID); ?></div>
+					<div class="legend cat"><?php echo strip_tags(get_the_term_list($post->ID, 'categorie')); ?></div>
+				</div>
+
 			</div>
-		</section>
-    	<?php 
+			<?php
+				endwhile;
+				endif;
+				wp_reset_postdata();
+				// if($the_query->have_posts()):
+				// while ($the_query -> have_posts()) {
+				//     $the_query -> the_post();
+				//     // Variables de stockage
+				//     $refPhoto = get_field("reference");
+				//     $cat = get_the_terms($post, "categorie");
+				//     $catname = $cat[0]->name;	
 
-		// Gallerie
-		
-		get_template_part('template-parts/home' , 'gallery');
+				//     // Mise en place du hover
+				//     echo '<div class="hover-photo">';
+				//     the_post_thumbnail(); 
+				//     echo '<div class="lightbox-hover">';
+				//     // Récupération de l'image + titre + catégorie
+				//     echo '<img class="zoom lightbox-open" data-category='. $catname .' data-reference='. $refPhoto .' data-image='. get_the_post_thumbnail_url() .'  src="' . get_template_directory_uri() . '/assets/images/Icon_fullscreen.png" >';
+				//     echo '<a href=" '. get_the_permalink() .' "><img class="oeil" src="' . get_template_directory_uri() . '/assets/images/Icon_eye.png" ></a>';
+				//     echo '<div class="legend-align">';
+				//     echo '<div class="legend ref-photo">' . get_the_title() . '</div>';
+				//     echo '<div class="legend cat">' . $catname . '</div>';
+				//     echo '</div>';
+				//     echo '</div>';
+				//     echo '</div>';
+				// // } 
+				// wp_reset_postdata();
+			?>
+				
 
-		?>
-		
-        <!-- Charger plus de posts (pagination) -->
+			</div>
+			<?php 
 
-        <!-- Montrer 8 premières photos par défaut -->
-        <?php 
-        $publications = new WP_Query([
-        'post_type' => 'photos',
-        'posts_per_page' => 8,
-        'orderby' => 'date',
-        'order' => 'DESC',
-        'paged' => 1,
-        ]);
-        ?>
-        <!-- Boucle sur les résultats de la requète $publications -->
-        <?php if($publications->have_posts()): ?>
-        <ul class="publication-list">
-            <?php 
-            while ($publications->have_posts()): $publications->the_post();
-                get_template_part('parts/card', 'publication');
-            endwhile;
-            ?>
-        </ul>
-        <?php endif; ?>
-        <?php wp_reset_postdata(); ?>
-        <!-- Bouton "Charger plus" -->
-        <div class="btn__wrapper">
-        <a href="#!" class="btn btn__primary" id="load-more">Charger plus</a>
-        </div>
+			// Gallerie
+			
+			// get_template_part('template-parts/home' , 'gallery');
+
+			?>
+			
+			<!-- Charger plus de posts (pagination) -->
+
+			<!-- Montrer 8 premières photos par défaut -->
+			<?php  ?>
+			
+
+			
+			<!-- Bouton "Charger plus" -->
+			<div class="btn__wrapper">
+			<input type="button" Value="Charger plus" class="btn btn_loadMore" id="load-more">
+			</div>
 		</div>
 	</section>
 
