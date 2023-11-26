f65bdf325afc74ab1a86b3da5b219b5d58a8306f
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-11-26 17:26:09 +0100

    Modif JS lightbox

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -1,91 +1,72 @@
-// Prendre le lightbox et les éléments de l'overlay
-var lightbox = document.querySelector('#myLightbox');
-jQuery(document).ready(function() {
-    // Activation du lightbox par l'image zoom
-    var zoomLightbox = document.querySelectorAll('.lightbox-open');
+// Déclarez images en dehors de $(document).ready
+let images;
 
-    // Ouverture de la lightbox au clic sur l'image zoom
-    zoomLightbox.forEach(function(zoom, index) {
-        zoom.addEventListener('click', function(e) {
-            // Eviter le comportement par défaut de l'évènement (ex: éviter que le navigateur accède à une autre page que la page actuelle )
-            e.preventDefault();
-            // Affichage du lightbox
-            lightbox.style.display = "block";
-        });
-    });
-});
+jQuery(document).ready(function ($) {
+    let currentIndex = 0;
+    const lightbox = $('#custom-lightbox');
+    const lightboxImage = $('#lightbox-image');
+    const closeLightboxButton = $('.close-lightbox');
+    const lightboxTextDiv = $('.lightboxText');
 
-// Fermeture de la lightbox au clic sur la croix
-document.querySelector('.exit-lightbox').addEventListener('click', function(e) {
-    e.preventDefault();
-    lightbox.style.display="none";
-});
+    // Assurez-vous que images est initialisé avec les éléments .lightbox-open
+    images = $('.lightbox-open');
 
+    // Affiche la lightbox
+    function openLightbox(index) {
+        currentIndex = index;
+        updateLightboxContent();
+        updatelightboxTextContent();
+        lightbox.show();
+    }
 
-// Essai
+    // Ferme la lightbox
+    function closeLightbox() {
+        lightbox.hide();
+    }
 
-// Déclarer les photos
-// let images; 
+    // Met à jour le contenu de la lightbox avec l'image actuelle
+    function updateLightboxContent() {
+        const currentImage = images.eq(currentIndex);
+        const imageUrl = currentImage.attr('data-image');
+        lightboxImage.attr('src', imageUrl);
+    }
 
-// $(document).ready(function() {
-//     var currentIndex = 0;
-//     const lightbox = $('#custom-lightbox');
-//     const lightboxImage = $('#lightbox-image');
-//     const closeLightboxButton = $('.exit-lightbox');
-//     const lightboxTextDiv = $('.legend-lb-align');
-    
+    // Met à jour le contenu de la div avec la catégorie et la référence
+    function updatelightboxTextContent() {
+        const currentImage = images.eq(currentIndex);
+        const category = currentImage.attr('data-category');
+        const reference = currentImage.attr('data-reference');
 
-    // Initialisation des images avec la classe 'full screen'
-    // images = $('.zoom-image');
+        console.log('Category:', category);
+        console.log('Reference:', reference);
 
+        const categoryText = category ? `<p> ${category}</p>` : '';
+        const referenceText = reference ? `<p> ${reference}</p>` : '';
 
+        lightboxTextDiv.html(categoryText + referenceText);
+    }
 
-
-    // MAJ du contenu de la lightbox avec l'image actuelle
-    // function updateLightboxContent() {
-    //     const currentImage = images.eq(currentIndex);
-    //     const imageUrl = currentImage.attr('data-image');
-    //     lightboxImage.attr('src', imageUrl);
-    // }
-
-    // MAJ du contenu de la div avec la catégorie et la référence
-    // function updatelightboxTextContent() {
-    //     const currentImage = images.eq(currentIndex);
-    //     const category = currentImage.attr('data-category');
-    //     const reference = currentImage.attr('data-reference');
-
-    //     console.log('Category:', category);
-    //     console.log('Reference:', reference);
-
-    //     const categoryText = category ? `<p> ${category}</p>` : '';
-    //     const referenceText = reference ? `<p> ${reference}</p>` : '';
-
-    //     lightboxTextDiv.html(categoryText + referenceText);
-    // }
-
-    // Change l'image dans la lightbox en fonction de la direction (gauche ou droite)
-    // function changeImage(direction) {
-    //     currentIndex = (currentIndex + direction + images.length) % images.length;
-    //     updateLightboxContent();
-    //     updatelightboxTextContent();
-    // }
+    // Change l'image dans la lightbox en fonction de la direction (prev ou next)
+    function changeImage(direction) {
+        currentIndex = (currentIndex + direction + images.length) % images.length;
+        updateLightboxContent();
+        updatelightboxTextContent();
+    }
 
     // Événements - utilise la délégation d'événements
-//     $(document).on('click', '.zoom-image', function () {
-//         openLightbox(images.index(this));
-//     });
+    $(document).on('click', '.lightbox-open', function () {
+        openLightbox(images.index(this));
+    });
 
-//     closeLightboxButton.on('click', closeLightbox);
+    closeLightboxButton.on('click', closeLightbox);
 
-//     // Gérer l'événement du bouton "précédent"
-//     $('.flechegauche-overlay-inactive').on('click', function () {
-//         changeImage(-1);
-//     });
+    // Gérer l'événement du bouton "précédent"
+    $('.prev').on('click', function () {
+        changeImage(-1);
+    });
 
-//     // Gérer l'événement du bouton "suivant"
-//     $('.flechedroite-overlay-inactive').on('click', function () {
-//         changeImage(1);
-//     });
-
-// });
-
+    // Gérer l'événement du bouton "suivant"
+    $('.next').on('click', function () {
+        changeImage(1);
+    });
+});
