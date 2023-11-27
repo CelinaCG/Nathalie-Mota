464e82c14fd46f2a67a47cfa484abd1bf80a6516
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-11-27 17:47:27 +0100

    MAJ contenu

--- template-parts/home-gallery.php
+++ template-parts/home-gallery.php
@@ -2,37 +2,38 @@
 <div class="col-photo">
 
     <?php 
-        $current_category = get_the_terms(get_the_ID(), 'categorie');
         $the_query = new WP_Query(array(
             'post_type' => 'photos',
             'posts_per_page' => 12,
+            'post__not_in' => array(get_the_ID()),
+            'paged' => 1
         ));
 
         if($the_query->have_posts()):
-        while ($the_query -> have_posts()) {
-            $the_query -> the_post();
-            // Variables de stockage
-            $refPhoto = get_field("reference");
-            $cat = get_the_terms($post, "categorie");
-            $catname = $cat[0]->name;	
+            while ($the_query -> have_posts()) {
+                $the_query -> the_post();
+                // Variables de stockage
+                $refPhoto = get_field("reference");
+                $cat = get_the_terms($post, "categorie");
+                $catname = $cat[0]->name;	
 
-            // Mise en place du hover
-            echo '<div class="hover-photo">';
-            the_post_thumbnail(); 
-            echo '<div class="lightbox-hover">';
-            // Récupération de l'image + titre + catégorie
-            echo '<img class="zoom lightbox-open" data-category='. $catname .' data-reference='. $refPhoto .' data-image='. get_the_post_thumbnail_url() .'  src="' . get_template_directory_uri() . '/assets/images/Icon_fullscreen.png" >';
-            echo '<a href=" '. get_the_permalink() .' "><img class="oeil" src="' . get_template_directory_uri() . '/assets/images/Icon_eye.png" ></a>';
-            echo '<div class="legend-align">';
-            echo '<div class="legend ref-photo">' . get_the_title() . '</div>';
-            echo '<div class="legend cat">' . $catname . '</div>';
-            echo '</div>';
-            echo '</div>';
-            echo '</div>';
-        } 
-        wp_reset_postdata();
+                // Mise en place du hover
+                echo '<div class="hover-photo">';
+                the_post_thumbnail(); 
+                echo '<div class="lightbox-hover">';
+                // Récupération de l'image + titre + catégorie
+                echo '<img class="zoom lightbox-open" data-category='. strip_tags(get_the_term_list(get_the_ID(), 'categorie')) .' data-reference='. get_field('reference', get_the_ID()) .' data-image='. get_the_post_thumbnail_url() .'  src="' . get_template_directory_uri() . '/assets/images/Icon_fullscreen.png" >';
+                echo '<a href=" '. get_the_permalink() .' "><img class="oeil" src="' . get_template_directory_uri() . '/assets/images/Icon_eye.png" ></a>';
+                echo '<div class="legend-align">';
+                echo '<div class="legend ref-photo">' . get_the_title() . '</div>';
+                echo '<div class="legend cat">' . strip_tags(get_the_term_list($post->ID, 'categorie')) . '</div>';
+                echo '</div>';
+                echo '</div>';
+                echo '</div>';
+            } 
+            wp_reset_postdata();
     ?>
-    <?php else : ?>
+        <?php else : ?>
     <p><?php esc_html_e( 'Désolé, il n\'y a aucun post qui correspond à vos critères.' ); ?></p>
     <?php endif; ?>
 </div>
