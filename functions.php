118284f1a6b5f4511efd9682f8b72dc0cfe1afa9
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-11-27 17:45:43 +0100

    Ajout fichiers

--- functions.php
+++ functions.php
@@ -32,7 +32,7 @@ add_action('customize_register', 'montheme_customize_register');
 // Ajout style CSS et script JQuery
 function ajout_CSS_script() {
     // JQuery
-    wp_enqueue_script('jquery');
+    wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.6.0.min.js', array(), '3.6.0', true);
     // JS
     wp_enqueue_script('script', get_template_directory_uri() . '/assets/js/script.js', array('jquery'), '1.0', true);
     // CSS
@@ -40,6 +40,7 @@ function ajout_CSS_script() {
     // Lightbox
     wp_enqueue_script('script-lightbox', get_template_directory_uri() . '/assets/js/lightbox.js', array('jquery'), '1.0', true);
 	wp_enqueue_style('photoEvent-lightbox-style', get_template_directory_uri() . '/assets/css/lightbox.css');
+	wp_enqueue_script('photoEvent-ajax', get_template_directory_uri() . '/assets/js/ajax.js', array(), '1.0', true);
 }
 add_action( 'wp_enqueue_scripts', 'ajout_CSS_script' );
 
@@ -77,7 +78,7 @@ echo '<a id="contact" href="' . esc_url($template_part) .'">Contact</a>';
 
 // Filtre Pagination
 
-function filtre() {
+function gallery_load_more() {
 	// Boucle de requète pour filtrer éléments en fonction des paramètres POST
 	$filtre = new WP_Query([
 	  'post_type' => 'photos',
@@ -110,12 +111,11 @@ function filtre() {
 		
 	?>
 			<div class="hover-photo">
-				<?php the_content(); ?>
+				<?php echo the_post_thumbnail(); ?>
 				<div class="lightbox-hover">
-					<a href="#">
-						<img class="p-ecran zoom lightbox-open zoom-image" data-category="<?php echo strip_tags(get_the_term_list(get_the_ID(), 'categorie')); ?>" data-reference="<?php echo get_field('reference', get_the_ID()); ?>" data-image="<?php echo get_the_post_thumbnail_url(); ?>" src="<?php echo get_template_directory_uri(); ?>/assets/images/Icon_fullscreen.png" alt="full_screen">
-					</a> <a href="<?php echo get_the_permalink(get_the_ID()); ?>">
-						<img class="oeil" src="<?php echo get_template_directory_uri(); ?>/assets/images/Icon_eye.png" alt="eye">
+					<img class="zoom lightbox-open zoom-image" data-category="<?php echo strip_tags(get_the_term_list(get_the_ID(), 'categorie')); ?>" data-reference="<?php echo get_field('reference', get_the_ID()); ?>" data-image="<?php echo get_the_post_thumbnail_url(); ?>" src="<?php echo get_template_directory_uri(); ?>/assets/images/Icon_fullscreen.png" alt="image plein écran">
+					<a href="<?php echo get_the_permalink(get_the_ID()); ?>">
+						<img class="oeil" src="<?php echo get_template_directory_uri(); ?>/assets/images/Icon_eye.png" alt="icône oeil">
 					</a>
 					<div class="legend-align">
 						<div class="legend ref-photo"><?php echo get_field('reference', $post->ID); ?></div>
@@ -130,11 +130,11 @@ function filtre() {
 		wp_reset_postdata();
 		exit();
 	}
-	add_action('wp_ajax_filtre', 'filtre');
-	add_action('wp_ajax_nopriv_filtre', 'filtre');
+add_action('wp_ajax_gallery_load_more', 'gallery_load_more');
+add_action('wp_ajax_nopriv_gallery_load_more', 'gallery_load_more');
 
 	
-/** Fonction pour afficher les options de catégorie pour le filtrage **/
+// Fonction pour afficher les options de catégorie du filtre
 function filtreCategorie()
 {
 	if ($terms = get_terms(array(
@@ -146,7 +146,7 @@ function filtreCategorie()
 			echo '<option  value="' . $term->slug . '">' . $term->name . '</option>';
 		}
 }
-/** Fonction pour afficher les options de format pour le filtrage **/
+// Afficher les options de format du filtre
 function filtreFormat()
 {
 	if ($terms = get_terms(array(
@@ -158,7 +158,7 @@ function filtreFormat()
 			echo '<option  value="' . $term->slug . '">' . $term->name . '</option>';
 		}
 }
-/** Fonction pour afficher les options de tri pour le filtrage **/
+// Afficher les options de tri du filtre
 function filtreOrderDirection()
 {
 	if ($order_options = (array(
